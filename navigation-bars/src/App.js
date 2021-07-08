94ade7433931aabc5e4ca365977100db21db99fd
Murat <mrtgms3561@gmail.com>
2021-07-08 23:07:58 +0300

    React Sidebar Navigation with using Routers

--- navigation-bars/src/App.js
+++ navigation-bars/src/App.js
@@ -1,5 +1,10 @@
 
 import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Home from './pages/Home'
+import Reports from './pages/Reports'
+import Products from './pages/Products'
 import NavigationBar from './components/NavigationBar';
 
 
@@ -7,7 +12,16 @@ import NavigationBar from './components/NavigationBar';
 const App = () => {
   return (
     <div className="App">
-      <NavigationBar/>
+      {/* <NavigationBar/> */}
+
+      <Router>
+          <Navbar/>
+          <Switch>
+            <Route path='/' exact component={Home}/>
+            <Route path='/reports' component={Reports}/>
+            <Route path='/products' component={Products}/>
+          </Switch>
+      </Router>
     </div>
   );
 }
